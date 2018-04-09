a84900b9aee7641ad2625d9faad84dfe0b040bca
Andrey <magot.a.d@gmail.com>
2018-04-09 18:37:55 +0300

    add bin/page-loader.js

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -1,1 +1,15 @@
-#! usr/bin/env node
+#! /usr/bin/env node
+import program from 'commander';
+import pageLoader from '../';
+import { version } from '../../package.json';
+
+program
+  .description('description')
+  .version(version)
+  .arguments('<url>')
+  .option('-o, --output', 'path to html')
+  .action((url) => {
+    const dir = program.output ? program.output : process.cwd();
+    pageLoader(url, dir);
+  })
+  .parse(process.argv);
