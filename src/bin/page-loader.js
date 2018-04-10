f90a729a5d54530bb8f7b8913dd1ffcd220d531b
Andrey <magot.a.d@gmail.com>
2018-04-10 14:12:26 +0300

    refactor path handling

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -1,6 +1,5 @@
 #! /usr/bin/env node
 import program from 'commander';
-import path from 'path';
 import pageLoader from '../';
 import { version } from '../../package.json';
 
@@ -8,9 +7,6 @@ program
   .description('Downloads a page from the Web and all the resourses associated with it for the offline use.')
   .version(version)
   .arguments('<url>')
-  .option('-o, --output [path]', 'path to the destination folder')
-  .action((url) => {
-    const dirName = path.normalize(program.output || '');
-    pageLoader(url, dirName);
-  })
+  .option('-o, --output [path]', 'path to the destination folder', '.')
+  .action(url => pageLoader(url, program.output))
   .parse(process.argv);
