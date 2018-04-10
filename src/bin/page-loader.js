e4de60be259f1b60641ddb8975e0f54ea05ba3e5
Andrey <magot.a.d@gmail.com>
2018-04-10 04:29:46 +0300

    add description

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -5,10 +5,10 @@ import pageLoader from '../';
 import { version } from '../../package.json';
 
 program
-  .description('description')
+  .description('Downloads a page from the Web and all the resourses associated with it for the offline use.')
   .version(version)
   .arguments('<url>')
-  .option('-o, --output [path]', 'path to html')
+  .option('-o, --output [path]', 'path to the destination folder')
   .action((url) => {
     const dirName = path.normalize(program.output || '');
     pageLoader(url, dirName);
